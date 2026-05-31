649a34fa0f63c18c9f86a03714ae1c79f39479d7
AnaMOrales <ana.morales@estudiantes.utec.edu.uy>
2026-05-30 21:58:38 -0300

    Sanctum

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,8 +2,10 @@
 
 use App\Providers\AppServiceProvider;
 use App\Providers\BroadcastServiceProvider;
+use Laravel\Sanctum\SanctumServiceProvider;
 
 return [
     AppServiceProvider::class,
     BroadcastServiceProvider::class,
+    SanctumServiceProvider::class,
 ];
